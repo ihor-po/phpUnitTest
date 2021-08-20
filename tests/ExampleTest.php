b9da1ff9d98ff4bc4bcf00f1dca8450bcaa34f7f
Igor Porotikov <sobikashi@gmail.com>
2021-08-20 10:34:52 +0300

    Update PHPUnit and add mockery library

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,6 +4,11 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
     public function testAddingTwoPlusTwoResultsInFour()
     {
         $this->assertEquals(4, 2 + 2);
--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -4,6 +4,11 @@ use PHPUnit\Framework\TestCase;
 
 class OrderTest extends TestCase
 {
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
     public function testOrderIsProcessed()
     {
         $gateway = $this->getMockBuilder('PaymentGateway')
@@ -14,6 +19,22 @@ class OrderTest extends TestCase
                 ->method('charge')
                 ->with(100)
                 ->willReturn(true);
+
+        $order = new Order($gateway);
+
+        $order->amount = 100;
+
+        $this->assertTrue($order->process());
+    }
+
+    public function testOrderIsProcessedUsingMockery()
+    {
+
+        $gateway = Mockery::mock('PaymentGateway');
+        $gateway->shouldReceive('charge')
+                ->once()
+                ->with(100)
+                ->andReturn(true);
 
         $order = new Order($gateway);
 
